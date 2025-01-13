5cb28159c3bd488a4a83b2786c8048064132e4a2
Illia Halushkin <galushkinilia@gmail.com>
2025-01-13 20:36:17 +0100

    move all exercises to folder

--- part_2_1/ex3.py
+++ part_2_1/ex3.py
@@ -5,7 +5,6 @@ You can assume the number given by the user is always greater than zero.
 
 An example of expected behaviour:
 
-Sample output
 Please type in a number: 1.34
 Integer part: 1
 Decimal part: 0.34

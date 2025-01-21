093825fe5083335524ad1bf0d8c1e9d767d18e77
Illia Halushkin <galushkinilia@gmail.com>
2025-01-21 22:27:04 +0100

    ex5 completed

---
+++ part_2_4/ex5.py
@@ -0,0 +1,34 @@
+"""
+Please write a program which keeps asking the user for a PIN code until they type in the correct one, which is 4321. The program should then print out the number of times the user tried different codes.
+
+Sample output
+PIN: 3245
+Wrong
+PIN: 1234
+Wrong
+PIN: 0000
+Wrong
+PIN: 4321
+Correct! It took you 4 attempts
+
+If the user gets it right on the first try, the program should print out something a bit different:
+
+Sample output
+PIN: 4321
+Correct! It only took you one single attempt!
+"""
+
+attempt = 0
+
+while True:
+    attempt += 1
+    pin = int(input("PIN: "))
+    if pin == 4321:
+        if attempt == 1:
+            print("Correct! It only took you one single attempt!")
+            break
+        else:
+            print(f"Correct! It took you {attempt} attempts")
+            break
+    else:
+        print("Wrong")

8a439da480dacc110b3838c8b1dce7abf1f9d541
Illia Halushkin <galushkinilia@gmail.com>
2025-02-01 21:30:45 +0100

    ex5 completed

---
+++ part_3_2/ex5.py
@@ -0,0 +1,17 @@
+"""
+Please write a program which prints out a line of hash characters, the width of which is chosen by the user.
+
+Sample output
+Width: 3
+
+###
+Sample output
+Width: 8
+
+########
+"""
+
+width = int(input("Width: "))
+hash = "#"
+
+print(hash * width)
